e715592f7012ed8a6996bbddf9be540f957b21b0
Lightbulb Games Studio <naji-newman-2000@hotmail.com>
2016-02-04 19:31:43 +0200

    Create commonwords.py

---
+++ elementary/commonwords.py
@@ -0,0 +1,11 @@
+def checkio(first, second):
+    first = first.split(',')
+    second = second.split(',')
+    words = []
+    res = ''
+    for x in first:
+        if x in second:
+            words.append(x)
+    for y in sorted(words):
+        res += (','+y if y != words[0] else y)
+    return res
