802b30548686f18b22a84fbba0f96c77c4a92327
Lightbulb Games Studio <naji-newman-2000@hotmail.com>
2016-02-04 21:15:58 +0200

    Update housepassword.py

--- home/housepassword.py
+++ home/housepassword.py
@@ -1,6 +1,4 @@
-import re
-
 def checkio(data):
-    if  any(char.isdigit() for char in data)and any(char.isalpha() for char in data) and len(data)>= 10 :
+    if  any(char.isdigit() for char in data)and any(char.isalpha() for char in data)and any(char.isupper() for char in data)and any(char.islower() for char in data) and len(data)>= 10 :
         return True
     return False
